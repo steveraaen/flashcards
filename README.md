1e44b74f68a97d9672384c24a87a7b420f3c2fb4
steveraaen <sraaen@gmail.com>
2017-02-21 15:13:16 -0500

    Finished.  Cards.txt created dynamically and ready to be populated with front and back regardless of whether input was cloze or basic.  Commented with usage instructions and updated readme.

--- README.md
+++ README.md
@@ -1,31 +1,8 @@
 # flashcards
 
-- Two kinds of flashcards, basic and cloze.
-- For basic:
-	inquirer prompt that asks if user wants to add a card or play.		
-			user selects add:
-				inquirer prompt asks for the front (clue)					
-				inquirer prompt asks for the back (answer)
-					save text of response as 'cardFront' and 'cardBack' object
-					stringify object and push to array 'flashcards'
-			user selects play:
-				console.log front of the first card
-				inquirer prompt 'ready to see the answer?'
-					console log back of first card
-				inquirer prompt 'next card'?
-					if Y, repeat process for second card
-					if N, console log goodbye
-
-- For Cloze:
-	inquirer prompt that asks if user wants to add a card or play.	
-		user selects add:
-			inquirer prompt asks for the whole sentence
-					save text of response as 'clozeCard'
-					stringify object and push to array 'flashcards'
-		user selects play:
-			function to console log "... was the"
-			inquirer prompt 'ready to see the answer?'
-				console.log whole sentence
-				inquirer prompt 'next card'?
-					if Y, repeat process for second card
-					if N, console log goodbye
+This is a command line interface.  Use argv[2] = 'b' construct a basic card, 
+and argv[2] = 'c' for a cloze card.  Prototype function MakeCloze.prototype.clozeOnly
+creates the cloze deleted text and adds it as a proto property (as well as the cloze 
+and full text).  Function cToB uses those proto properties to build a new basic card 
+with only front and back properties. So that function writeCard is used to add all
+cards, regardless of how they were created to the same 'cards.txt' file
--- proto.js
+++ proto.js
@@ -1,7 +1,15 @@
+// This is a command line interface.  Use argv[2] = 'b' construct a basic card, 
+// and argv[2] = 'c' for a cloze card.  Prototype function MakeCloze.prototype.clozeOnly
+// creates the cloze deleted text and adds it as a proto property (as well as the cloze 
+// and full text).  Function cToB uses those proto properties to build a new basic card 
+// with only front and back properties. So that function writeCard is used to add all
+// cards, regardless of how they were created to the same 'cards.txt' file
+
 var fs = require('fs');
 // 2 kinds of cards, basic and cloze
 var bCard;
 var cCard;
+var cardStr;
 // First command line input defines which card to build
 var func = process.argv[2];
 // Assignments for basic
@@ -12,84 +20,55 @@ var fullText = process.argv[3];
 var cloze = process.argv[4];
 var ellipse = '...';
 var clozeToBasic;
-var cardStr;
-
-function writeCard(){
-    fs.appendFile('cards.txt', cardStr + '\r', function(err){
+// function to write new card to file
+function writeCard() {
+    fs.appendFile('cards.txt', cardStr + '\r', function(err) {
         if (err) throw err;
     })
 }
+// Make basic card if argv[2] = 'b'
 if (func === 'b') {
     function MakeBasic(f, b) {
-        if (!(this instanceof MakeBasic)){
+        if (!(this instanceof MakeBasic)) {
             return new MakeBasic(front, back)
         }
         this.front = front;
         this.back = back;
     }
     bCard = new MakeBasic(front, back);
+
+    // stringify and write basic card
     cardStr = JSON.stringify(bCard);
+    console.log('New basic card added fo file' + cardStr)
     writeCard();
-} else if(func === 'c') {
-    function MakeCloze(text){
-        if (!(this instanceof MakeCloze)){
+    // Make cloze card if argv[2] = 'c'
+} else if (func === 'c') {
+    function MakeCloze(text) {
+        if (!(this instanceof MakeCloze)) {
             return new MakeCloze(fullText)
         }
-        this.fullText = fullText;    
-        this.cloze = cloze; 
-
+        this.fullText = fullText;
+        this.cloze = cloze;
     }
     cCard = new MakeCloze(fullText);
     cardStr = JSON.stringify(cCard);
+    // Create partial text property & add as .__proto__. properties
 
-
-
-MakeCloze.prototype.clozeOnly = function(){
+    MakeCloze.prototype.clozeOnly = function() {
         MakeCloze.prototype.cloze = cloze;
         MakeCloze.prototype.partial = fullText.replace(cloze, ellipse);
         MakeCloze.prototype.fullText = fullText;
+    }
+    cCard.clozeOnly()
+        // function to convert cloze to basic by simply running the original MakeBasic constructor 
+        //and writeCard functions
+    function cToB() {
+        back = cCard.__proto__.fullText;
+        front = cCard.__proto__.partial;
+        clozeToBasic = MakeBasic(back, front)
+        cardStr = JSON.stringify(clozeToBasic)
+        writeCard(cardStr)
+    }
+    cToB()
+    console.log('new basic card created from cloze inputs'  + cardStr);
 }
-cCard.clozeOnly()
-
-function cToB(){
-back = cCard.__proto__.fullText;
-front = cCard.__proto__.partial;
-clozeToBasic = MakeBasic(back, front)
-cardStr = JSON.stringify(clozeToBasic)
-writeCard()
-}
-cToB()
-console.log(clozeToBasic);
-}
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
