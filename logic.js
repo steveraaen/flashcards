d7c726b12c92bef66a02cd0549747dcef816d4c7
steveraaen <sraaen@gmail.com>
2017-02-21 14:33:48 -0500

    cleaned up

--- logic.js
+++ logic.js
@@ -1,61 +1,97 @@
-var fs = require('fs');
+var fs = var fs = require('fs');
 // 2 kinds of cards, basic and cloze
-var bCard = {};
-var cCard = {};
+var bCard;
+var cCard;
 // First command line input defines which card to build
 var func = process.argv[2];
 // Assignments for basic
 var front = process.argv[3];
 var back = process.argv[4];
 // Assignments for cloze
-var full = process.argv[3];
+var fullText = process.argv[3];
 var cloze = process.argv[4];
-var remain = process.argv[5];
+var ellipse = '...';
+var clozeToBasic;
 var cardStr;
-// Choose basic or cloze
+
+function writeCard(){
+    fs.appendFile('cards.txt', cardStr + '\r', function(err){
+        if (err) throw err;
+    })
+}
 if (func === 'b') {
-    basicCard()
-}
-else if (func === 'c'){
-    clozeCard()
-}
-// Construct scope safe basic object 
-function basicCard(){
-    function MakeBasic(f, b){
-        if(!(this instanceof MakeBasic)){
+    function MakeBasic(f, b) {
+        if (!(this instanceof MakeBasic)){
             return new MakeBasic(front, back)
         }
         this.front = front;
         this.back = back;
     }
-MakeBasic.call(bCard, front, back)
-/*    bCard = new MakeBasic(front, back)*/
-// Save basic card to ext file
-        cardStr = JSON.stringify(bCard);
-        fs.appendFile('cards.txt', cardStr + '\r', function(err){
-            if (err) throw err;
-        })   
-}
-// Construct scope safe cloze object 
-function clozeCard(){
-    function MakeCloze(f, c, r){
-        if(!(this instanceof MakeCloze)){
-            return new MakeCloze(full, cloze, remain)
+    bCard = new MakeBasic(front, back);
+    cardStr = JSON.stringify(bCard);
+    writeCard();
+} else if(func === 'c') {
+    function MakeCloze(text){
+        if (!(this instanceof MakeCloze)){
+            return new MakeCloze(fullText)
         }
-        this.full = full;
-        this.cloze = cloze;
-        this.remain = remain;
+        this.fullText = fullText;    
+        this.cloze = cloze; 
+
     }
-    cCard = new MakeCloze(full, cloze, remain)
-// Save cloze card to ext file
-        cardStr = JSON.stringify(cCard);
-        fs.appendFile('cards.txt', cardStr + '\r', function(err){
-            if (err) throw err;
-        })   
+    cCard = new MakeCloze(fullText);
+    cardStr = JSON.stringify(cCard);
+
+
+
+MakeCloze.prototype.clozeOnly = function(){
+        MakeCloze.prototype.cloze = cloze;
+        MakeCloze.prototype.partial = fullText.replace(cloze, ellipse);
+        MakeCloze.prototype.fullText = fullText;
+}
+cCard.clozeOnly()
+
+function cToB(){
+back = cCard.__proto__.fullText;
+front = cCard.__proto__.partial;
+clozeToBasic = MakeBasic(back, front)
+cardStr = JSON.stringify(clozeToBasic)
+writeCard()
+}
+cToB()
+console.log(clozeToBasic);
 }
 
-var bProto = Object.getPrototypeOf(bCard);
-console.log(bProto)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 
